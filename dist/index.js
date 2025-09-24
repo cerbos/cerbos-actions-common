708729f6c3ad3fa8cb9a0549a4029483e3b38689
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2025-09-24 10:57:23 +0300

    enhancement: Add upload command (#3)
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
 export * from './compile.js';
 export * from './setup.js';
+export * from './upload.js';
 //# sourceMappingURL=index.js.map
---
+++ dist/upload.js
@@ -0,0 +1,61 @@
+import * as core from '@actions/core';
+import * as available from './available.js';
+import * as child from 'child_process';
+import * as z from 'zod';
+const binaryCerbosctl = 'cerbosctl';
+const workspaceEnvKey = 'GITHUB_WORKSPACE';
+const argsSchema = z.object({
+    clientID: z.string().nonempty(),
+    clientSecret: z.string().nonempty(),
+    storeID: z.string().nonempty(),
+    fromRevision: z.string().default('HEAD~1'),
+    toRevision: z.string().default('HEAD'),
+    subDir: z.string().optional()
+});
+const validateArgs = (args) => {
+    return argsSchema.parse(args);
+};
+export const upload = async (args) => {
+    args = validateArgs(args);
+    const av = await available.available({
+        binary: binaryCerbosctl
+    });
+    if (!av.inPath) {
+        core.setFailed(`Failed to find binary ${binaryCerbosctl} in PATH, please add https://github.com/cerbos/cerbos-setup-action action to your workflow.`);
+        process.exit(1);
+    }
+    const workspaceDir = process.env[workspaceEnvKey];
+    if (!workspaceDir) {
+        core.setFailed(`Environment variable ${workspaceEnvKey} is not set by the workflow runner.`);
+        process.exit(1);
+    }
+    let command = `${av.path} upload-git ${args.fromRevision} ${args.toRevision} --path=${workspaceDir} --store-id=${args.storeID} --client-id=${args.clientID} --client-secret=${args.clientSecret}`;
+    if (args.subDir && args.subDir !== '') {
+        core.info(`Subdirectory is set to ${args.subDir}`);
+        command += ` --subdir ${args.subDir}`;
+    }
+    else {
+        core.info('Subdirectory is not set');
+    }
+    core.info(`The command to run is: ${command}`);
+    core.startGroup(`The results for the command ${command}`);
+    child.exec(command, (err, stdout, stderr) => {
+        let exitCode = 0;
+        if (err) {
+            exitCode = 1;
+            core.setFailed(`Failed to run Cerbosctl`);
+            core.error(err.message);
+        }
+        if (stderr) {
+            exitCode = 1;
+            core.setFailed(`An error occured`);
+            core.error(stderr);
+        }
+        if (stdout) {
+            core.info(stdout);
+        }
+        core.endGroup();
+        process.exit(exitCode);
+    });
+};
+//# sourceMappingURL=upload.js.map
