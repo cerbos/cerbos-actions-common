9e3df2e3718c322aa2435daaf53914e6828ce0f1
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2025-11-13 11:58:30 +0300

    enhancement: Make from and to arguments optional
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

--- dist/types/upload.d.ts
+++ dist/types/upload.d.ts
@@ -3,8 +3,8 @@ interface Args {
     clientID: string;
     clientSecret: string;
     storeID: string;
-    fromRevision: string;
-    toRevision: string;
+    fromRevision?: string;
+    toRevision?: string;
     subDir?: string;
 }
 export declare const upload: (args: Args) => Promise<void>;
